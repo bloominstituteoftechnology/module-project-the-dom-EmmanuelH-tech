af7102484d0295dbdea137920567ec87b06cbf3e
Emmanuel Henry <EmmanuelHenrytech@gmail.com>
2024-06-11 16:44:58 -0400

    finish

--- frontend/index.js
+++ frontend/index.js
@@ -6,19 +6,94 @@ function moduleProject1() {
   // 👇 WORK WORK BELOW THIS LINE 👇
 
   // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
-  //  ✨ add your code here
+  const widgets = document.querySelectorAll('section>div')
+
+  widgets.forEach((widget, idx) =>{
+    widget.classList.add('widget')
+    widget.setAttribute('tabindex', idx + 1)
+  })
 
   // 👉 TASK 2 - Build a "Quote of the Day" widget
-  //  ✨ add your code here
+  const randomIdx = Math.floor(Math.random() * quotes.length) //eslint-disable-line
+  const randomQuote = quotes[randomIdx] //eslint-disable-line
+  const quote = document.createElement('div')
+  const quoteText = randomQuote.quote
+  quote.textContent = quoteText
+  const firstWid = document.querySelector('.quoteoftheday')
+  firstWid.appendChild(quote)
+  const authorDate = document.createElement('div')
+  const { author, date } = randomQuote
+  authorDate.textContent = `${author} in ${date || 'an unknown date'}`
+  firstWid.appendChild(authorDate)
+  
 
   // 👉 TASK 3 - Build a "Corporate Speak" widget
-  //  ✨ add your code here
+  const randomVerb1 = verbs [ Math.floor(Math.random() * verbs.length)]
+  const randomVerb2 = verbs [ Math.floor(Math.random() * verbs.length)]
+
+  const randomAdverb1 = adverbs [ Math.floor(Math.random() * adverbs.length)]
+  const randomAdverb2 = adverbs [ Math.floor(Math.random() * adverbs.length)]
+
+  const randomNoun1 = nouns [ Math.floor(Math.random() * nouns.length)]
+  const randomNoun2 = nouns [ Math.floor(Math.random() * nouns.length)]
+
+  const mumboJumbo = `We need to ${randomVerb1} our ${randomNoun1} ${randomAdverb1} in order to ${randomVerb2} our ${randomNoun2} ${randomAdverb2}.`
+const paragraph = document.createElement('p')
+paragraph.textContent = mumboJumbo
+document.querySelector('.corporatespeak').appendChild(paragraph)
+  
+
+
+  
 
   // 👉 TASK 4 - Build a "Countdown" widget
-  //  ✨ add your code here
+  const countdownWid = document.querySelector('.countdown')
+  let count = 5
+  const countdown = document.createElement('p')
+  countdown.textContent = `T-minus ${count}...`
+  countdownWid.appendChild(countdown)
+
+  const id = setInterval(() => {
+    if (count === 1) {
+      countdown.textContent = 'Liftoff! 🚀'
+      clearInterval(id)
+    } else {
+      countdown.textContent = `T-minus ${--count}...`
+    }
+  }, 1000);
 
   // 👉 TASK 5 - Build a "Friends" widget
-  //  ✨ add your code here
+  const randomPerson = people[Math.floor(Math.random() * people.length)]
+  const personParagraph = document.createElement('p')
+  document.querySelector('.friends').appendChild(personParagraph)
+  const year = randomPerson.dateOfBirth.split('-')[0]
+  
+  let sentence = `${randomPerson.fname} ${randomPerson.lname} was born in ${year} and `
+
+   if (!randomPerson.friends.length) {
+    sentence += 'has no friends.'
+   } else {
+    sentence += 'is friends with '
+    for (let idx = 0; idx < randomPerson.friends.length; idx++){
+      const friendId = randomPerson.friends[idx]
+      const friend = people.find(p => p.id === friendId)
+      const fullName = `${friend.fname} ${friend.lname}`
+    console.log(fullName)
+    let isLastIdx = idx === randomPerson.friends.length - 1
+    let isNextLastIdx = idx === randomPerson.friends.length - 2
+    if (isLastIdx) {
+      sentence += `${fullName}.`
+    } else if (isNextLastIdx) {
+      sentence += `${fullName} and `
+    } else {
+      sentence += `${fullName}, `
+    }
+      }
+   }
+  personParagraph.textContent = sentence
+  console.log(randomPerson)
+  
+
 
   // 👉 TASK 6 - Make it so user can tab through the widgets
   //  ✨ add your code here
